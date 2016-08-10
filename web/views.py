ecef95f4d2bb47ca624fef84630514a89f5c41d0
Leif Coleman <mageleif@yahoo.com>
2016-08-10 11:40:15 -0500

    Rebase files

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'web'}
+@view_config(route_name="index", renderer="templates/index.jinja2")
+def index(request):
+    debug = "Hello World"
+    return {"debug": debug}
