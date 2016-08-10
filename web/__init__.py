3ad11dbc810ce8c0f8369779e87d9185552aa4f4
Leif Coleman <mageleif@yahoo.com>
2016-08-10 13:20:28 -0500

    Add functionality

--- web/__init__.py
+++ web/__init__.py
@@ -1,23 +1,15 @@
 from pyramid.config import Configurator
 
 
-
 def main(global_config, **settings):
-
-
     config = Configurator(settings=settings)
-
 
     config.include("pyramid_jinja2")
 
     config.add_static_view("static", "static", cache_max_age=3600)
 
     config.add_route("index", "/")
-
-
-
-
-
+    config.add_route("readfile", "/readfile")
 
     config.scan()
     return config.make_wsgi_app()
--- web/views.py
+++ web/views.py
@@ -1,7 +1,31 @@
 from pyramid.view import view_config
+import json
+import os
 
 
 @view_config(route_name="index", renderer="templates/index.jinja2")
 def index(request):
-    debug = "Hello World"
-    return {"debug": debug}
+    data = {"html": ""}
+    year = "2016"
+    week = "32"
+    server = "212914826169679872"
+    str = "year" + "-" + "week" + "-" + "server"
+
+    for file in os.listdir("log"):
+        name = file[8:26]
+        if name == server:
+            channel = file[27:-4]
+            data["html"] += """<button type="button" onclick="ReadFile(this);"
+                            file="{}">{}</button>""".format(file, channel)
+
+    return data
+
+
+@view_config(route_name="readfile", renderer="json")
+def readfile(request):
+    file = request.POST.get("file")
+    data = {"text": "Error"}
+    with open("log/" + file, "r") as f:
+        data["text"] = f.read()
+
+    return data
